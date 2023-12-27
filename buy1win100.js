d6c963fc1d43a61c7fd31d8b868183e128aa6717
Raybin King <raybinking@gmail.com>
2023-12-27 15:48:45 +0800

    add buy1win100

---
+++ buy1win100.js
@@ -0,0 +1,98 @@
+// Create global userWalletAddress variable
+window.userWalletAddress = null;
+
+//================================
+// when the browser is ready
+//================================
+window.onload = async (event) => {
+	// contract address
+    Xmas_address = "0x2606138c9D1Bb59A29df9586Aa925eA7E185B80a";
+	// abi
+    Xmas_abi = [
+        {"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},
+        {"constant":false,"inputs":[{"name":"_spender","type":"address"},
+    	{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},
+    	{"constant":true,"inputs":[],"name":"initialized","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},
+    	{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoSetting","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoRandomNo","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoPool","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoWinner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoCountDown","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoAdds","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},
+    	{"constant":true,"inputs":[],"name":"lotoPlayerNo","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+    	{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},
+    	{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},
+    	{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},
+    	{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}
+    ];    
+    
+	// create contract instance
+    contract = new web3.eth.Contract(Xmas_abi, Xmas_address);
+	
+    // show the user dashboard
+    showUserDashboard();
+};
+
+//================================
+// function to show dashboard
+//================================
+const showUserDashboard = async () => {
+    get_liquidity();
+    get_lotoPool();
+    get_lotoPlayer();
+    get_lotoWinner();
+    // Initialize Snowfall
+    snowStorm();
+};
+
+// get liquidity
+const get_liquidity = async () => {
+	Xmas_liquidityAddress = "0x6EE90044a9a7e634DbfF7F83Cf05F30887Ae1faF";
+	Xmas_liquidity = await contract.methods.balanceOf(Xmas_liquidityAddress).call();
+	Xmas_liquidity -= Xmas_liquidity % 100000000;
+	Xmas_liquidity = Xmas_liquidity / 100000000;
+    Xmas_liquidity = Number(Xmas_liquidity).toString();
+    document.querySelector(".liquidity").innerHTML = Xmas_liquidity;
+}
+
+// get loto pool
+const get_lotoPool = async () => {
+	Xmas_poolAddress = "0x467c1D872B7e7cc68121324479f3691bD3052ba8";
+	Xmas_lotoPool = await contract.methods.balanceOf(Xmas_poolAddress).call();
+	Xmas_lotoPool -= Xmas_lotoPool % 100000000;
+	Xmas_lotoPool = Xmas_lotoPool / 100000000;
+    Xmas_lotoPool = Number(Xmas_lotoPool).toString();
+    document.querySelector(".loto-pool").innerHTML = Xmas_lotoPool;
+}
+
+// get loto player
+const get_lotoPlayer = async () => {
+	Xmas_lotoPlayer = await contract.methods.lotoPlayerNo().call();
+    Xmas_lotoPlayer = Number(Xmas_lotoPlayer).toString();
+    document.querySelector(".loto-player").innerHTML = Xmas_lotoPlayer;
+}
+
+// get loto winner
+const get_lotoWinner = async () => {
+	Xmas_Address0 = "0x0000000000000000000000000000000000000000";
+	Xmas_lotoWinner = await contract.methods.lotoWinner().call();
+	if (Xmas_lotoWinner == Xmas_Address0) {
+	    document.querySelector(".loto-winner").innerHTML = 0;
+	}
+	else {
+        document.querySelector(".loto-winner").innerHTML = Xmas_lotoWinner;
+    }
+}
+
